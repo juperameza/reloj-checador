a197043cef1e125fa2dc8eb93b12774db2b00046
Alejandro Curiel <alex_curielg@hotmail.com>
2022-03-10 19:25:45 -0600

    Created the model and migration for "Employee"
    Co-authored-by: juperameza <juperameza@hotmail.com>:wq

---
+++ db/migrate/20220311005632_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :email
+      t.string :position
+      t.integer :private_number
+      t.references :branch, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
