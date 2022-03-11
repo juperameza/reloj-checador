55318a54c9b7e4c5b8866308c1078b66a5438e39
Alejandro Curiel <alex_curielg@hotmail.com>
2022-03-10 19:13:43 -0600

    Add model and migration for "Admin"
    Co-authored-by: juperameza <juperameza@hotmail.com>

---
+++ db/migrate/20220310235942_create_admins.rb
@@ -0,0 +1,13 @@
+class CreateAdmins < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admins do |t|
+      t.string :name
+      t.string :email
+      t.string :position
+      t.integer :private_number
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
