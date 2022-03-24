2ae8918a11d6d000eb149988c9fff40e3a21fe97
germanruzca <germanruzca@gmail.com>
2022-03-23 19:17:07 -0600

    Add data in the seed file
    Co-authored-by: juperameza <juperameza@hotmail.com>
    Co-authored-by: nallely64 <nallelygar.64@gmail.com>
    Co-authored-by: AlejandroCuriel <alex_curielg@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# StreetCp.create(street_name: "Calle", cp: "28000")
+# Branch.create(name: "Calle", street_cp_id: 1, city: "Colima", town: "Villa De Alvarez", number: 955555555)
+# Employee.create(name: "Juan", email: "juan@gmail.com", position: "Gerente", private_number: 123456789, branch_id: 1)
+# Admin.create(email: "admin@admin.com", encrypted_password: "admin123")
