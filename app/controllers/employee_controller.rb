f5f41ef98a681d02d5d2d421a7fa687a4faa1683
juperameza <juperameza@hotmail.com>
2022-03-30 16:15:38 -0600

    Add the create method and new view for employee
    Co-authored-by: nallely64 <nallelygar.64@gmail.com>
    Co-authored-by: AlejandroCuriel <alex_curielg@hotmail.com>
    Co-authored-by: germanruzca <germanruzca@gmail.com>

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,7 +1,31 @@
 class EmployeeController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @employees = Employee.includes(:branch)
   end
+
+  def new
+    @employee = Employee.new()
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def employee_params
+    params.require(:employee).permit(:name, :email, :private_number, :position, :branch_id)
+  end
+
+  # def set_employee
+  #   @employee = Employee.find(params[:id])
+  # end
 end
