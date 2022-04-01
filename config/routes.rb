239a94331317b1bb05732dc7ccecf55717358e2c
juperameza <juperameza@hotmail.com>
2022-03-31 19:44:13 -0600

    Implement the routes and methods for street
    Co-authored-by: nallely64 <nallelygar.64@gmail.com>
    Co-authored-by: AlejandroCuriel <alex_curielg@hotmail.com>
    Co-authored-by: germanruzca <germanruzca@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :branches
+  resources :street_cps
   devise_for :admins
   resources :employees do
     member do
       put :update_status
     end
   end
-  resources :branch
   root 'home#index'
   post "/", to:"home#create"
 end
