a82987cbe680aa6f3bca866a4e4621b878944d59
nallely64 <nallelygar.64@gmail.com>
2022-04-02 13:22:48 -0600

    Add check out logic for the employee
    Co-authored-by: juperameza <juperameza@hotmail.com>
    Co-authored-by: AlejandroCuriel <alex_curielg@hotmail.com>
    Co-authored-by: germanruzca <germanruzca@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,25 @@
 class HomeController < ApplicationController
-  #Si existe el numero privado del usuario
-  #Registrar fecha de registro y mostrar mensaje de confirmacion
-  #Sino existe, mostrar mensaje de error
   def index
   end
 
   def create
     @employee = Employee.find_by(private_number: params[:private_number])
-    if @employee
+    @employee ? @assistence = Assistence.find_by(employee_id: @employee.id, created_at: Time.current.all_day ) : nil
+    
+    if !@employee
+      flash[:msg] = "Employee number not found"
+      redirect_to root_path
+    elsif @employee && !@assistence 
       @assistance = Assistence.new(employee_id:@employee.id,clock_in:Time.now.strftime("%H:%M"))
       @assistance.save
-      flash[:msg] = "Assistance registered"
+      flash[:msg] = "Check in registered at #{Time.now.strftime("%H:%M")}"
       redirect_to root_path
-    else
-      flash[:msg] = "Employee number not found"
+    elsif !@assistence.clock_out 
+      @assistence.update_attribute(:clock_out, Time.now.strftime("%H:%M"))
+      flash[:msg] = "Check out registered at #{Time.now.strftime("%H:%M")}"
+      redirect_to root_path
+    else 
+      flash[:msg] = "<3 You are already registered, come back tomorrow <3"
       redirect_to root_path
     end
   end
