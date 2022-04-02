e54cd14506f3e58578bc31c82f56d4fcd64d2594
Alejandro Curiel <alex_curielg@hotmail.com>
2022-04-01 18:45:12 -0600

    Add status property for logical erase
    Co-authored-by: juperameza <juperameza@hotmail.com>
    Co-authored-by: nallely64 <nallelygar.64@gmail.com>
    Co-authored-by: germanruzca <germanruzca@gmail.com>

--- db/migrate/20220311003928_create_branches.rb
+++ db/migrate/20220311003928_create_branches.rb
@@ -5,6 +5,7 @@ class CreateBranches < ActiveRecord::Migration[7.0]
       t.string :city
       t.string :town
       t.integer :number
+      t.boolean :status
       t.references :street_cp, null: false, foreign_key: true
 
       t.timestamps
