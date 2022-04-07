3f30b64ad08177bd999ab33a25158931cd5c1fe3
juperameza <juperameza@hotmail.com>
2022-04-07 16:59:52 -0500

    Added logic to count absences
    Implemented the logic to count absences by the day of the month that we
    are in

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
   belongs_to :branch
   has_many :assistence
+  def absences(id)
+    Date.today.day - Assistence.where(employee_id: id, clock_in: (Date.today.beginning_of_month..Date.today.end_of_month) ).count
+  end
 end
