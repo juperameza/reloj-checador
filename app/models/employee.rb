d1366eade2d8b736620ba77f92bc163ea092620c
germanruzca <germanruzca@gmail.com>
2022-04-07 23:01:53 -0500

    Create model method to return the assistance of the day

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,8 @@
 class Employee < ApplicationRecord
   belongs_to :branch
   has_many :assistence
+
+  def total_assistance
+    Assistence.where(employee_id: self.id, clock_in: (Date.today.beginning_of_month..Date.today.end_of_month) ).count
+  end
 end
