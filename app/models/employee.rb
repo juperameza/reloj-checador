7722a1542615bfb8d9ed2667794f670858d765cb
German Ruiz <germanruzca@gmail.com>
2022-04-09 20:22:43 -0500

    Update employee.rb

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,6 +4,7 @@ class Employee < ApplicationRecord
 
   def total_assistance
     Assistence.where(employee_id: self.id, clock_in: (Date.today.beginning_of_month..Date.today.end_of_month) ).count
+  end
 
   def absences(id)
     Date.today.day - Assistence.where(employee_id: id, clock_in: (Date.today.beginning_of_month..Date.today.end_of_month) ).count
