6ddc70d229ff9507eea7b1df4ef1471ad38df457
juperameza <juperameza@hotmail.com>
2023-02-16 14:04:51 -0600

    Fixing something

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,21 +5,25 @@ class HomeController < ApplicationController
   def create
     @employee = Employee.find_by(private_number: params[:private_number])
     @employee ? @assistence = Assistence.find_by(employee_id: @employee.id, created_at: Time.current.all_day ) : nil
-    
-    if !@employee
-      flash[:msg] = "Employee number not found"
-      redirect_to root_path
-    elsif @employee && !@assistence 
-      @assistance = Assistence.new(employee_id:@employee.id,clock_in:Time.now.strftime("%H:%M"))
-      @assistance.save
-      flash[:msg] = "Check in registered at #{Time.now.strftime("%H:%M")}"
-      redirect_to root_path
-    elsif !@assistence.clock_out 
-      @assistence.update_attribute(:clock_out, Time.now.strftime("%H:%M"))
-      flash[:msg] = "Check out registered at #{Time.now.strftime("%H:%M")}"
-      redirect_to root_path
-    else 
-      flash[:msg] = "<3 You are already registered, come back tomorrow <3"
+    if @employee.status
+      if !@employee
+        flash[:msg] = "Employee number not found"
+        redirect_to root_path
+      elsif @employee && !@assistence 
+        @assistance = Assistence.new(employee_id:@employee.id,clock_in:Time.now.strftime("%H:%M"))
+        @assistance.save
+        flash[:msg] = "Check in registered at #{Time.now.strftime("%H:%M")}"
+        redirect_to root_path
+      elsif !@assistence.clock_out 
+        @assistence.update_attribute(:clock_out, Time.now.strftime("%H:%M"))
+        flash[:msg] = "Check out registered at #{Time.now.strftime("%H:%M")}"
+        redirect_to root_path
+      else 
+        flash[:msg] = "<3 You are already registered, come back tomorrow <3"
+        redirect_to root_path
+      end
+    else
+      flash[:msg] = "Employee is not active"
       redirect_to root_path
     end
   end
